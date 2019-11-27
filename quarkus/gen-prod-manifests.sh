76b3c56ae19450aa54b0ff833fd48d6763ec9d01
Radu <39598837+radudd@users.noreply.github.com>
2019-11-27 22:17:39 +0100

    Update and rename gen-prod-secret.sh to gen-prod-manifests.sh

--- quarkus/gen-prod-manifests.sh
+++ quarkus/gen-prod-manifests.sh
@@ -5,13 +5,14 @@ if [[ $# -ne 2 ]]; then
 
 ERR: Please provide the API URL and the Namespace of the Prod Cluster as arguments
 
-  ./gen-prod-secret.sh <NAMESPACE_PROD_CLUSTER>
+  ./gen-prod-secret.sh <API_PROD_CLUSTER> <NAMESPACE_PROD_CLUSTER>
 
 EOF
   exit 99
 fi
 
-export NAMESPACE=$1
+export API_URL=$1
+export NAMESPACE=$2
 
 SERVICE_ACCOUNT_SECRET_NAME=$(oc get serviceaccount/pipeline -n ${NAMESPACE} -o jsonpath='{.secrets[0].name}')
 CA_DATA=$(kubectl get secret/$SERVICE_ACCOUNT_SECRET_NAME -n ${NAMESPACE} -o jsonpath="{.data.ca\.crt}")
@@ -26,6 +27,31 @@ type: Opaque
 data:
   cadataKey: $CA_DATA
   tokenKey: $TOKEN
+EOF
+
+cat <<EOF > pipelineresource-prod-cluster.yaml
+apiVersion: tekton.dev/v1alpha1
+kind: PipelineResource
+metadata:
+  name: prod-cluster
+spec:
+  type: cluster
+  params:
+    - name: name
+      value: prod-cluster
+    - name: username
+      value: pipeline
+    - name: namespace
+      value: $NAMESPACE
+    - name: url
+      value: $API_URL
+  secrets:
+    - fieldName: token
+      secretKey: tokenKey
+      secretName: cluster-prod-secret
+    - fieldName: cadata
+      secretKey: cadataKey
+      secretName: cluster-prod-secret
 EOF
 
 # Reference:
